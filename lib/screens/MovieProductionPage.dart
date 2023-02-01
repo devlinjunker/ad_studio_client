2b2424ae4031bf45ff7a10531f01856e3d1d7d7e
Devlin Junker <devlin.junker@gmail.com>
2023-02-01 08:24:33 -0800

    test modal

--- lib/screens/MovieProductionPage.dart
+++ lib/screens/MovieProductionPage.dart
@@ -50,6 +50,41 @@ class _MovieProductionPageState extends State<MovieProductionPage> {
         .setCurrentMoviePoster(image.data!.path);
   }
 
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Basic dialog title'),
+          content: const Text('A dialog is a type of modal window that\n'
+              'appears in front of app content to\n'
+              'provide critical information, or prompt\n'
+              'for a decision to be made.'),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Disable'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Enable'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Builder(builder: (context) {
@@ -129,7 +164,7 @@ class _MovieProductionPageState extends State<MovieProductionPage> {
             ),
             ActionButton(
               label: 'Generate Scandal',
-              onPressed: () => generatePoster(),
+              onPressed: () => _dialogBuilder(context),
               icon: const Icon(Icons.enhance_photo_translate),
             ),
             ActionButton(
